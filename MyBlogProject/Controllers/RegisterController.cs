2f2b40ce9394317e1fadbcb6f9b63b8ad365401f
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-11 22:55:16 +0300

    11.01.2025

---
+++ MyBlogProject/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+         
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writer.WriterAbout = "deneme";
+            writerManager.WriterAdd(writer);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
