b39b578b00d7dd756404ba1543b9d7509ce59b4b
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-15 18:11:10 +0300

    15.01.2025

---
+++ MyBlogProject/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactRepository());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            contactManager.ContactAdd(contact);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
