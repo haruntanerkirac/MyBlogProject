0570b5b80d68f19decd063adfdd2d8b952ddaaa6
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-20 18:07:58 +0300

    20.01.2025

--- MyBlogProject/Controllers/WriterController.cs
+++ MyBlogProject/Controllers/WriterController.cs
@@ -5,9 +5,28 @@ namespace MyBlogProject.Controllers
 {
     public class WriterController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
         }
     }
 }
---
+++ MyBlogProject/ViewComponents/Writer/WriterMessageNotification.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents.Writer
+{
+    public class WriterMessageNotification : ViewComponent
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
