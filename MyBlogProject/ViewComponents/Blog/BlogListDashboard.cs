849c9ae88acd03d2a9379d8bd9fc31069f76fdff
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-21 23:13:17 +0300

    21.01.2025

---
+++ MyBlogProject/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = blogManager.GetBlogListWithCategory().TakeLast(10);
+            return View(values);
+        }
+    }
+}
