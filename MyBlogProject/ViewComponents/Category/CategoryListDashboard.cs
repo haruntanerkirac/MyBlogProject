0b72384a59adbc340b6fb38d90ff38c3a5ffa917
haruntanerkirac <haruntanerkirac@gmail.com>
2025-01-22 16:07:26 +0300

    22.01.2025

---
+++ MyBlogProject/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = categoryManager.GetAll();
+            return View(values);
+        }
+    }
+}
