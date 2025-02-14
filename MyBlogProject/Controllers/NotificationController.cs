67eb39ead8cfb293b6535a8a65052c68fa82c194
haruntanerkirac <haruntanerkirac@gmail.com>
2025-02-14 16:01:50 +0300

    14.02.2025

--- MyBlogProject/Controllers/NotificationController.cs
+++ MyBlogProject/Controllers/NotificationController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyBlogProject.Controllers
 {
     public class NotificationController : Controller
     {
+        NotificationManager notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous ]
+        public IActionResult AllNotification()
+        {
+            var values = notificationManager.GetAll();
+            return View(values);
         }
     }
 }
