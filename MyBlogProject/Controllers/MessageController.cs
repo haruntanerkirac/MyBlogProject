ca6974b544f06d3164f892e3e581a02c02399a55
haruntanerkirac <haruntanerkirac@gmail.com>
2025-02-15 13:14:33 +0300

    15.02.2025

---
+++ MyBlogProject/Controllers/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyBlogProject.Controllers
+{
+    [AllowAnonymous]
+    public class MessageController : Controller
+    {
+        Message2Manager messageManager = new Message2Manager(new EfMessage2Repository());
+        public IActionResult InBox()
+        {
+            int id = 2;
+            var values = messageManager.GetInboxListByWriter(2);
+            return View(values);
+        }
+
+        public IActionResult MessageDetails(int id)
+        {
+            var value = messageManager.TGetById(id);
+            return View(value);
+        }
+    }
+}
