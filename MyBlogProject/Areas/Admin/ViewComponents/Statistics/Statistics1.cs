e86f9b715f822c9e3c43608f55bccf3716715282
haruntanerkirac <haruntanerkirac@gmail.com>
2025-02-16 20:53:31 +0300

    16.02.2025

---
+++ MyBlogProject/Areas/Admin/ViewComponents/Statistics/Statistics1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Areas.Admin.ViewComponents.Statistics
+{
+    public class Statistics1 : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = blogManager.GetAll().Count;
+            ViewBag.MessageCount = context.Contacts.Count();
+            ViewBag.CommentCount = context.Comments.Count();
+            return View();
+        }
+    }
+}
