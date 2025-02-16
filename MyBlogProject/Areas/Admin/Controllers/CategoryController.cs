001081811e2fb9ccf57a50b6e51f0c70672ba862
haruntanerkirac <haruntanerkirac@gmail.com>
2025-02-16 15:16:27 +0300

    16.02.2025

--- MyBlogProject/Areas/Admin/Controllers/CategoryController.cs
+++ MyBlogProject/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,13 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace MyBlogProject.Areas.Admin.Controllers
 {
@@ -8,10 +15,45 @@ namespace MyBlogProject.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page = 1)
         {
-            var values = categoryManager.GetAll();
+            var values = categoryManager.GetAll().ToPagedList(page, 3);
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            CategoryValidator validator = new CategoryValidator();
+            ValidationResult results = validator.Validate(category);
+            if (results.IsValid)
+            {
+                category.CategoryStatus = true;
+                categoryManager.TAdd(category);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = categoryManager.TGetById(id);
+            categoryManager.TDelete(value);
+            return RedirectToAction("Index");
         }
     }
 }
