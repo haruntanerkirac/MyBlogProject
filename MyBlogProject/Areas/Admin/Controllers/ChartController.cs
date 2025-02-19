c3472389f4fb5dacf2ff49d40f82bf7f24ed1847
haruntanerkirac <haruntanerkirac@gmail.com>
2025-02-19 22:44:14 +0300

    19.02.2025

---
+++ MyBlogProject/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogProject.Areas.Admin.Models;
+
+namespace MyBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass
+            {
+                categoryname = "Teknoloji",
+                categorycount = 10
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Yazılım",
+                categorycount = 20
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Spor",
+                categorycount = 30
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Sanat",
+                categorycount = 15
+            });
+
+            list.Add(new CategoryClass
+            {
+                categoryname = "Bilim",
+                categorycount = 5
+            });
+
+            return Json(new { jsonlist = list });
+        }
+    }
+}
---
+++ MyBlogProject/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlogProject.Areas.Admin.Models;
+using Newtonsoft.Json;
+
+namespace MyBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int writerId)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == writerId);
+            var jsonWriters = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass writer)
+        {
+            writers.Add(writer);
+            var jsonWriters = JsonConvert.SerializeObject(writer);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass { Id = 1, Name = "John Doe" },
+            new WriterClass { Id = 2, Name = "Jane Doe" },
+            new WriterClass { Id = 3, Name = "Jake Doe" }
+        };
+    }
+}
